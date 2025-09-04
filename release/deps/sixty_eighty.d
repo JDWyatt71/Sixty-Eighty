4a910daf21e6cde76a5d080dccc6a23a2e1b3748
JDWyatt71 <jdw231@exeter.ac.uk>
2025-09-04 15:37:19 +0100

    Delete release directory

--- release/deps/sixty_eighty.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Wyatt\OneDrive\Documents\rust_qm\sixty_eighty\target\release\deps\sixty_eighty.exe: src\main.rs
-
-C:\Users\Wyatt\OneDrive\Documents\rust_qm\sixty_eighty\target\release\deps\sixty_eighty.d: src\main.rs
-
-src\main.rs:
--- release/sixty_eighty.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Wyatt\OneDrive\Documents\rust_qm\sixty_eighty\target\release\sixty_eighty.exe: C:\Users\Wyatt\OneDrive\Documents\rust_qm\sixty_eighty\src\main.rs
